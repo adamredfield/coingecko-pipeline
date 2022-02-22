fccd8885735a786b7542406033952190c3714241
adam-redfield <82402243+adam-redfield@users.noreply.github.com>
2022-02-22 10:07:41 -0700

    removed workflows

--- src/rds_config.py
+++ src/rds_config.py
@@ -12,7 +12,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 
 logger = logging.getLogger()
-logging.basicConfig(filename=f'logs/data_{datetime.now()}.log', encoding='utf-8')
 logger.setLevel(logging.INFO)
 
 os.environ['AWS_PROFILE'] = "adam-aws"
